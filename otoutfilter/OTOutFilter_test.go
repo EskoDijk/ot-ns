689198f0fe944fd52d77a95f3aa441a46e5cb1e5
Esko Dijk <esko.dijk@iotconsultancy.nl>
2023-06-10 23:13:11 +0200

    [otoutfilter] log-filter updated for v1.1 node build.

--- otoutfilter/OTOutFilter_test.go
+++ otoutfilter/OTOutFilter_test.go
@@ -46,6 +46,10 @@ func TestOTOutFilter(t *testing.T) {
 		"D  33:00:17.817 [N] log4\n" +
 		"E text 44:33:22.123 [I]log5\n" +
 		"F text [x] no log\n" +
+		"00:00:00.000 [INFO]-CORE----: Notifier: StateChanged (0x01001009) [Ip6+ LLAddr Ip6Mult+ NetifState]\n" +
+		"00:00:00.000 [NOTE]-CLI-----: Output: > Done\n" +
+		"00:00:00.000 [DEBG]-PLAT----: Clear ExtAddr entries\n" +
+		"30:30:23.456 [WARN]-PLAT----: some text\n" +
 		"G[C]log2\n" +
 		"H[W] log3\n" +
 		"I[N] log4\n" +
