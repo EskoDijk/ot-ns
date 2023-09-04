2349f50f1c2fef885e52f106831a3e9f8843dcc0
Esko Dijk <esko.dijk@iotconsultancy.nl>
2023-09-04 17:56:48 +0200

    [all] make-pretty

--- dispatcher/FailureCtrl_test.go
+++ dispatcher/FailureCtrl_test.go
@@ -55,6 +55,9 @@ func (m mockDispatcherCallback) OnLogMessage(logEntry LogEntry) {
 func (m mockDispatcherCallback) OnNextEventTime(curTimeUs uint64, nextTimeUs uint64) {
 }
 
+func (m mockDispatcherCallback) OnStop() {
+}
+
 func TestFailureCtrlNonFailure(t *testing.T) {
 	node1 := &Node{
 		Id: 0x1,
