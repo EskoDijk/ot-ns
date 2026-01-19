dbf643374a52273c594f8a3a6cde5380ba01cb37
Esko Dijk <esko.dijk@iotconsultancy.nl>
2026-01-19 12:10:47 +0100

    WIP: GUI support fixes for real-time mode, for RCPs

--- visualize/types.go
+++ visualize/types.go
@@ -95,8 +95,9 @@ func DefaultTitleInfo() TitleInfo {
 	}
 }
 
+// NetworkInfo represents information about a simulated network (if InvalidNodeId) or node (if valid NodeId).
 type NetworkInfo struct {
-	Real          bool
+	Real          bool // Whether the node or network is simulated with fixed real-time speed
 	Version       string
 	Commit        string
 	NodeId        int
