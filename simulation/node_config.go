7d3158463ec10356f9fca947cadbddb81f11c6e0
Esko Dijk <esko.dijk@iotconsultancy.nl>
2026-03-09 18:03:35 +0100

    070-feat: support OT Posix node with RCP using realtime UART
    This adds support for running a standard OT Posix node in a realtime
    simulation, where the Posix process performs realtime SPINEL/HDLC UART
    communication with an RCP process. Support for RCPs is the basis for
    upcoming features like including real OTBRs in a simulation, either
    running on the local host or running in a Docker.
    
    The dispatcher/simulation core is updated to allow handling of both
    'realtime UART' (stdout) based CLI and logging interactions and
    'virtual-time UART' interactions by nodes, in the same simulation. The
    real-time simulation mode now has a different timekeeping mechanism
    that is synced to the host's clock and processes continuously, not
    based on discrete time periods or different speeds - like virtual-time
    simulation is. Deletion of nodes is updated in real-time mode: it is
    ongoing in the background in a separate goroutine, to ensure the
    simulation doesn't halt upon node deletion and that all final log
    messages of the exiting node are captured also.
    
    A method waitForSimulation() is introduced to be more clear when
    command processing needs to wait for simulation events to be processed
    first. It simplifies the code in expectEvent() and expectLine()
    functions also.
    
    Also, the queueing and filtering of UART/log output of OT nodes is
    simplified to make the code better readable and the UART data flow
    easier to understand and debug. Log lines are received over stderr
    from the OT Posix node. The logger package (parse.go) is updated to
    parse syslog style log lines, and unit tests are added for all parsing
    functions.
    
    The CLI (over PTY/UART/...) echo behavior differs for Linux Posix nodes
    that are controlled via stdin/stdout: there is no 'echo' behavior.
    Depending on OS, there could be echo. To handle all possible
    combinations of platform/node-type, there is a new function 'setupCli'
    added that autodetects whether echo is used for a node, or not, and
    whether the CLI connection is functional.
    
    The RFSIM radio C code is updated to avoid going to sleep when there
    are still OTNS events in the queue, received via the Unix socket.
    This ensures faster overall processing with less events being sent,
    relevant for nodes to keep up in real-time mode.
    
    Python unit tests are extended with a more strict error checking, to
    ensure that unexpected errors from OTNS cause the unit tests to fail.
    
    Finally, previous build errors for the rcp target (Issue #758) are now
    resolved.
    
    Closes #758
    
    Co-authored-by: François Michel <francoismichel.dev@gmail.com>

--- simulation/node_config.go
+++ simulation/node_config.go
@@ -30,6 +30,7 @@ import (
 	"fmt"
 	"math"
 	"os"
+	"os/exec"
 	"path/filepath"
 	"slices"
 	"strings"
@@ -93,6 +94,10 @@ var defaultBrScript = []string{
 	"br enable",
 }
 
+// defaultRcpScript is an array of additional commands, sent to any Posix+RCP based node (unless changed).
+var defaultRcpScript = []string{
+	"log level 5",
+}
 var defaultExtScript = defaultMtdInitScript
 
 // selfGenOmrBrScript is a script for a BR that detects no IPv6 infra and creates its own OMR prefix.
@@ -126,6 +131,8 @@ type ExecutableConfig struct {
 	Mtd         string
 	Br          string
 	Matter      string
+	Rcp         string
+	RcpHost     string
 	SearchPaths []string
 }
 
@@ -151,6 +158,8 @@ var DefaultExecutableConfig ExecutableConfig = ExecutableConfig{
 	Mtd:         "ot-cli-mtd",
 	Br:          "ot-cli-ftd_br",
 	Matter:      "ot-matter-node",
+	Rcp:         "ot-rcp",
+	RcpHost:     "ot-cli",
 	SearchPaths: defaultSearchPaths(),
 }
 
@@ -183,12 +192,14 @@ func DefaultNodeConfig() NodeConfig {
 		IsRaw:          false,
 		IsRouter:       true,
 		IsMtd:          false,
+		IsRcp:          false,
 		IsBorderRouter: false,
 		IsExternal:     false,
 		RxOffWhenIdle:  false,
 		NodeLogFile:    true,
 		RadioRange:     defaultRadioRange,
 		ExecutablePath: "",
+		HostExePath:    "",
 		Restore:        false,
 		InitScript:     []string{},
 		RandomSeed:     0, // 0 means not specified, i.e. truly unpredictable.
@@ -201,6 +212,7 @@ func DefaultNodeScripts() *YamlScriptConfig {
 		Mtd: strings.Join(defaultMtdInitScript, "\n"),
 		Ftd: strings.Join(defaultFtdInitScript, "\n"),
 		Br:  strings.Join(defaultBrScript, "\n"),
+		Rcp: strings.Join(defaultRcpScript, "\n"),
 		Ext: strings.Join(defaultExtScript, "\n"),
 		All: strings.Join(defaultAllInitScript, "\n"),
 	}
@@ -209,12 +221,20 @@ func DefaultNodeScripts() *YamlScriptConfig {
 // NodeConfigFinalize finalizes the configuration for a new Node before it's used to create it. This is not
 // mandatory to call, but a convenience method for the caller to avoid setting all details itself.
 func (s *Simulation) NodeConfigFinalize(nodeCfg *NodeConfig) {
+	// certain combinations are either not allowed, or not supported yet by OTNS
+	logger.AssertFalse((nodeCfg.IsRcp || nodeCfg.IsBorderRouter) && nodeCfg.IsMtd)
+
 	if nodeCfg.ID <= 0 {
 		nodeCfg.ID = s.genNodeId()
 	}
 
-	nodeCfg.UpdateNodeConfigFromType()
+	if err := nodeCfg.UpdateNodeConfigFromType(); err != nil {
+		logger.Errorf("Node config finalization error: %v", err)
+		nodeCfg.ExecutablePath = "InvalidNodeExecutable"
+		return
+	}
 	nodeCfg.ExecutablePath = s.cfg.ExeConfig.FindExecutableBasedOnConfig(nodeCfg)
+	nodeCfg.HostExePath = s.cfg.ExeConfig.FindHostExecutableBasedOnConfig(nodeCfg)
 
 	// check for an implicit Thread-version setting in the executable-selection.
 	if len(s.cfg.ExeConfig.Version) > 0 && len(nodeCfg.Version) == 0 {
@@ -222,7 +242,8 @@ func (s *Simulation) NodeConfigFinalize(nodeCfg *NodeConfig) {
 	}
 
 	// in case of specified simulation random seed, each node gets a PRNG-predictable random seed assigned.
-	if s.cfg.RandomSeed != 0 {
+	// RCPs and externally started nodes are exceptions.
+	if s.cfg.RandomSeed != 0 && !nodeCfg.IsExternal && !nodeCfg.IsRcp {
 		nodeCfg.RandomSeed = prng.NewNodeRandomSeed()
 	}
 
@@ -234,6 +255,8 @@ func (s *Simulation) NodeConfigFinalize(nodeCfg *NodeConfig) {
 			nodeCfg.InitScript = append(nodeCfg.InitScript, s.cfg.NewNodeScripts.BuildBrScript()...)
 		} else if nodeCfg.IsMtd {
 			nodeCfg.InitScript = append(nodeCfg.InitScript, s.cfg.NewNodeScripts.BuildMtdScript()...)
+		} else if nodeCfg.IsRcp {
+			nodeCfg.InitScript = append(nodeCfg.InitScript, s.cfg.NewNodeScripts.BuildRcpFtdScript()...)
 		} else {
 			nodeCfg.InitScript = append(nodeCfg.InitScript, s.cfg.NewNodeScripts.BuildFtdScript()...)
 		}
@@ -288,7 +311,8 @@ func (cfg *ExecutableConfig) SetVersion(version string, defaultConfig *Executabl
 		cfg.Mtd = defaultConfig.Mtd + "_" + version
 		cfg.Matter = defaultConfig.Matter + "_" + version
 	}
-	cfg.Br = defaultConfig.Br // BR is currently not adapted to versions.
+	cfg.Br = defaultConfig.Br   // BR is currently not adapted to versions.
+	cfg.Rcp = defaultConfig.Rcp // RCP is currently not adapted to versions.
 	cfg.Version = version
 }
 
@@ -299,8 +323,13 @@ func isFile(exePath string) bool {
 	return false
 }
 
-// FindExecutable returns a full path to the named executable, by searching in standard search paths
-// if needed. If the given exeName is already a full path itself, or empty, it will be returned itself.
+func isFileExecutable(exePath string) bool {
+	_, err := exec.LookPath(exePath)
+	return err == nil
+}
+
+// FindExecutable returns a full path to the named executable, by searching in standard
+// search paths if needed. If the given exeName is already a full path itself, or empty, it will be returned itself.
 func (cfg *ExecutableConfig) FindExecutable(exeName string) string {
 	if len(exeName) == 0 || filepath.IsAbs(exeName) || exeName[0] == '.' {
 		return exeName
@@ -333,9 +362,25 @@ func (cfg *ExecutableConfig) FindExecutableBasedOnConfig(nodeCfg *NodeConfig) st
 	if nodeCfg.IsBorderRouter {
 		exeName = cfg.Br
 	}
+	if nodeCfg.IsRcp {
+		exeName = cfg.Rcp
+	}
 
 	if len(nodeCfg.Version) > 0 && nodeCfg.Version != versionLatestTag {
 		exeName += "_" + nodeCfg.Version
+	}
+
+	return cfg.FindExecutable(exeName)
+}
+
+// FindHostExecutableBasedOnConfig gets the RCP host executable, if any, based on NodeConfig information.
+func (cfg *ExecutableConfig) FindHostExecutableBasedOnConfig(nodeCfg *NodeConfig) string {
+	if len(nodeCfg.HostExePath) > 0 {
+		return nodeCfg.HostExePath
+	}
+	exeName := ""
+	if nodeCfg.IsRcp {
+		exeName = cfg.RcpHost
 	}
 
 	return cfg.FindExecutable(exeName)
